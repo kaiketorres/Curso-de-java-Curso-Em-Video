dcc95ec4f013ca7ff9d296c894844e54d4c85052
kaiketorres <kaiketorresdasilva@gmail.com>
2025-01-10 21:54:49 -0300

    Criada aplicação com JFrame e JLabel
    - Desenvolvido um exemplo básico usando Java Swing.
    - Implementado um JFrame com título "Minha Aplicação Swing".
    - Adicionado um JLabel com o texto "Olá, Swing!" e posição definida.
    - Código criado como parte do estudo inicial sobre GUI em Java.

---
+++ OlaMundo/src/main/java/OlaMundo.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Minha Aplicação Swing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+
+        JLabel label = new JLabel("Olá, Swing!");
+        frame.add(label);
+
+
+        label.setBounds(100, 80, 120, 30);
+
+
+        frame.setVisible(true);
+    }
+}
